407ab7b97315150b52908661995b91f57dde9502
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-06-15 20:30:04 +0530

    add search2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Search from "./components/Search";
+import Search from "./components/Search2";
 
 function App() {
   return <Search/>;
