088bd030d87b4f60226a425ff6e5db1302dbaef0
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-06-15 20:46:39 +0530

    add search3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Search from "./components/Search2";
+import Search from "./components/Search3";
 
 function App() {
   return <Search/>;
---
+++ src/components/Search3.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Search3 extends React.Component {
+    handleKeyDown = e => {
+        if(e.keyCode === 13){
+            const value = e.target.value;
+            alert("The value of the input field is " + value);
+        } 
+    }
+    render(){
+        return <div>
+            <input onKeyDown={this.handleKeyDown} type="text" name="username" placeholder="Enter Username"></input>
+        </div>
+    }
+}
+
+export default Search3;
