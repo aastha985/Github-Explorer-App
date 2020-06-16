881e6f7896614bc540e5f293281b8dd4f170a646
aastha985 <55681187+aastha985@users.noreply.github.com>
2020-06-16 13:37:24 +0530

    hadle errors,implement loader,display user details

--- src/App.js
+++ src/App.js
@@ -2,30 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Search from "./components/Search";
+import UserCard from "./components/UserCard";
 
 class App extends React.Component{
   state = {
     user: null,
+    error: null,
+    loading: false,
   };
 
   fetchUserData = async username => {
-    try{
-      const res = await fetch(`https://api.github.com/users/${username}`);
-      if(res.ok){
-        const data = await res.json();
-        return this.setState({
-          user: data
+    this.setState({
+      loading: true,
+    },async ()=>{
+      try{
+        // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        // const res = await fetch(proxyurl+`https://api.github.com/users/${username}`);
+        const res = await fetch(`https://api.github.com/users/${username}`);
+        if(res.ok){
+          const data = await res.json();
+          return this.setState({
+            user: data,
+            loading: false,
+          });
+        }
+        const error = (await res.json()).message;
+        this.setState({
+          error,
+          loading: false,
+        });
+  
+  
+      }
+      catch(err){
+        this.setState({
+          error: "There was some error",
+          loading: false,
         });
       }
-    }
-    catch(err){
-      console.log(err);
-    }
+    });
+    
     
   };
 
   render(){
-    return <Search fetchData={this.fetchUserData}/>;
+    // console.log(this.state);
+    const {error,loading,user} = this.state;
+    return <div>
+        <Search fetchData={this.fetchUserData}/>
+        {(loading && (<p>Loading...</p>))}
+        {error && <p className="text-danger">{error}</p>}
+        {!loading && !error && user && <UserCard user={user}/>}
+    </div>
   }
 }
 
---
+++ src/components/UserCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserCard = ({user}) => {
+    console.log({user});
+    return (<div className="card">
+        <div className="card-body">
+            <img src={user.avatar_url} alt="" />
+            <h1>{user.name}</h1>
+            <p>{user.company}</p>
+            <p>{user.bio}</p>
+        </div>
+    </div>);
+};
+
+export default UserCard;
